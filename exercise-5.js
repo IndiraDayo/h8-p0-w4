2aa8689665015865f8529622e57c3f66ef280760
IndiraDayo <indiradayo@live.com>
2018-06-28 14:08:04 +0700

    tambah test case

--- exercise-5.js
+++ exercise-5.js
@@ -15,3 +15,10 @@ function ubahHuruf(kata) {
   }
   return newString
 }
+
+// TEST CASES
+console.log(ubahHuruf('wow')); // xpx
+console.log(ubahHuruf('developer')); // efwfmpqfs
+console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+console.log(ubahHuruf('keren')); // lfsfo
+console.log(ubahHuruf('semangat')); // tfnbohbu
